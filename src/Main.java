d45a1a77c45ae772b0edd6b5b5f6bfc86d85084c
anna-ermakova <korovanna8@mail.ru>
2022-09-24 20:37:38 +0300

    ДЗ 2 задание 5

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,14 @@ public class Main {
         System.out.println(friend);
         friend=(friend+2)/7;
         System.out.println(friend);
+        //task 5
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
     }
 }
