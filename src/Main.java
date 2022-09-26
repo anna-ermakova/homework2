733eb78226e9e8eb9ae44d9371590a23241df5c7
anna-ermakova <korovanna8@mail.ru>
2022-09-26 19:13:02 +0300

    изменения в задаче 6

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         //task 6
+        System.out.println("Задача 6");
         var boxersWeight1=78.2;
         var boxersWeight2=82.7;
-        System.out.println(boxersWeight1+boxersWeight2);
-        System.out.println(boxersWeight2-boxersWeight1);
+        System.out.println("Общий вес двух боксеров: "+(boxersWeight1+boxersWeight2));
+        System.out.println("Разница в весе боксеров: "+(boxersWeight2-boxersWeight1));
+
         //task 7
+        System.out.println("Задача 7");
         System.out.println(boxersWeight2-boxersWeight1);
         System.out.println(boxersWeight2%boxersWeight1);
-        //task 8.1
+        //task 8
+        System.out.println("Задача 8,1");
         var totalWorkingHours=640;
         var everyEmployeeWorkingHours=8;
         var totalEmployees=totalWorkingHours/everyEmployeeWorkingHours;
         System.out.println( "Всего работников в компании – "+ totalEmployees+" человек");
-        //task 8.2
+        System.out.println("Задача 8,2");
         totalEmployees=totalEmployees+94;
         var newTotalWorkingHours=totalEmployees*everyEmployeeWorkingHours;
         System.out.println("Если в компании работает "+totalEmployees+" человек, то всего "+newTotalWorkingHours+" часов работы может быть поделено между сотрудниками");
